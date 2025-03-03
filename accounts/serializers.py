5ae087ec7f224261c6716e7dbcdd95cc2387bc1f
habibulmursaleen@gmail.com <habibulmursaleen@gmail.com>
2025-03-03 16:15:26 +0600

    minimal change

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import authenticate
-
+#minimal change
 
 
 
